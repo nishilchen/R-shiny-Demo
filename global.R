dd58eae3707e75121f9ecaf38e10d1ad0f1e28de
nishilchen <nishil0510@gmail.com>
2018-06-13 23:44:24 -0400

    'temp1'

--- global.R
+++
@@ -1,3 +0,0 @@
-library(tidyverse)
-kchouse <- read_csv(file = "data/kc_house_data.csv",
-                    col_names = TRUE)
---
+++ kchouse_app/global.R
@@ -0,0 +1,3 @@
+library(tidyverse)
+kchouse <- read_csv(file = "data/kc_house_data.csv",
+                    col_names = TRUE)
